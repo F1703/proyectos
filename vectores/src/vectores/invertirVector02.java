3bc8a468b30872db684de442ad6473b5ec2b2821
franco <franco@root>
2016-08-25 20:15:42 -0300

    b

--- vectores/src/vectores/invertirVector02.java
+++ vectores/src/vectores/invertirVector02.java
@@ -5,6 +5,8 @@
  */
 package vectores;
 
+import java.util.Random;
+
 /**
  *
  * @author franco
@@ -13,38 +15,41 @@ public class invertirVector02 {
     public static void main(String[] args) {
         int [] v1={2,4,6,8};
         int [] v2=new int[v1.length];
-        
-//        for (int i = 0; i < v1.length ; i++) {
-//            int ran=(int)(Math.random()*(v1.length-i)+1);
-//            int aux = v1[ran];
-//            v2[i]   =  aux;
-//            v1[ran] = 1;
-//            
-//        }
-//        System.out.println("-----");
-//        for (int i = 0; i < v2.length; i++) {
-//            System.out.println(v2[i]);
-//        }
-        System.out.println("");
-        for (int i = 0; i < v1.length; i++) {
-            int ran=(int)(Math.random()*(v1.length-1)+1);
-            System.out.println("ran:"+ran);
-            boolean ban=false;
-            for (int j = 0; j < v2.length; j++) {
-                if (v1[ran] == v2[j]) {
-                    System.out.println("   ::"+v1[ran]+" = "+v2[j]);
-                    ban=true;
-                    break;
-                }   
-            }
-            if (ban==false) {
-                v2[i] = v1[ran];
-            }
+        int tama=v1.length;
+        Random r=new Random();
+        int ran;
+        for (int i = 0; i < v1.length ; i++) {
+//            int ran=(int)(Math.random()*(tama-1)+1);
+            ran=r.nextInt(tama);
+            v2[i]   =  v1[ran];
+            v1[ran] = v1[tama-1];
+            tama--;
             
         }
-        System.out.println(" v2.................");
+        System.out.println("-----");
         for (int i = 0; i < v2.length; i++) {
             System.out.println(v2[i]);
         }
+//        System.out.println("");
+//        int tam=v1.length;
+//        for (int i = 0; i < v1.length; i++) {
+//            int ran=(int)(Math.random()*(tam-1)+1);
+//            System.out.println("ran:"+ran);
+//            boolean ban=false;
+//            for (int j = 0; j < v2.length; j++) {
+//                if (v1[ran] == v2[j]) {
+//                    System.out.println("   ::"+v1[ran]+" = "+v2[j]);
+//                    ban=true;
+//                    break;
+//                }   
+//            }
+//            if (ban==false) {
+//                v2[i] = v1[ran];
+//            }
+//        }
+//        System.out.println(" v2.................");
+//        for (int i = 0; i < v2.length; i++) {
+//            System.out.println(v2[i]);
+//        }
     }
 }
