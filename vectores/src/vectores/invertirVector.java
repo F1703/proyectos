8a5505ae90882a5b599b3ea47260dd692fc90b78
franco <franco@root>
2016-08-25 17:57:39 -0300

    invertirvectorddo

---
+++ vectores/src/vectores/invertirVector.java
@@ -0,0 +1,20 @@
+/*
+
+invertir un vector 
+ */
+package vectores;
+
+/**
+ *
+ * @author franco
+ */
+public class invertirVector {
+    public static void main(String[] args) {
+        
+        int [] v={1,3,5,6};
+        for (int i = v.length-1; i >= 0; i--) {
+            System.out.println(v[i]);
+        }
+        
+    }
+}
