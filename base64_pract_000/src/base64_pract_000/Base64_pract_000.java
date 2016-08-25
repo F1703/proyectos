ec32c6696fecb7e9a9acfa5a146a52f79af0aa5e
franco <franco@root>
2016-08-25 12:41:12 -0300

    subiendo 00

---
+++ base64_pract_000/src/base64_pract_000/Base64_pract_000.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base64_pract_000;
+
+/**
+ *
+ * @author franco
+ */
+public class Base64_pract_000 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
